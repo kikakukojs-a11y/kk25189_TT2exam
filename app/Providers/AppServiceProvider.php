d753b2792a2921fc32878dd983e17d9e09f72550
Kristiāns Kūkojs <kikakukojs0@gmail.com>
2026-06-19 01:32:12 +0300

    Fixed error that crashed live deplyoment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 use DeepL\Translator;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,20 +25,5 @@ class AppServiceProvider extends ServiceProvider
     }
     }
     
-}
-function __($text) {
-    $locale = app()->getLocale();
-    
-    if ($locale === 'en' || empty($text)) {
-        return $text;
-    }
-
-    $cacheKey = 'deepl_' . $locale . '_' . md5($text);
-
-    return Cache::rememberForever($cacheKey, function () use ($text, $locale) {
-        $translator = new Translator(config('services.deepl.key'));
-        $__d = $translator->__Text($text, 'en', $locale);
-        return $__d->text;
-    });
 }
 
